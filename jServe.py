fc6aa6703a4826b1150495d4f4ab372cdd543dde
tpapadop <44737157+tpapadop@users.noreply.github.com>
2019-01-25 15:53:11 -0500

    Updated for CORS

--- jServe.py
+++ jServe.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 import socket, ssl, sys, threading, re, json
-from urllib.parse import urlparse, unquote_plus
+from urllib.parse import urlparse
 from datetime import datetime
-from dicttoxml import dicttoxml
 
 # Single Serve Socket Server, recieve command and return result closing client socket
 # multiple connections handled via threads
 
 PORT = 9699 # the default port number to run our server on
-HOST = socket.gethostname() #use default hostname to run on
+HOST = socket.gethostname() # the default hostname to run on
 
-# CERTFILE = '/home/TPapad01/work/etc/cert.pem'
-CERTFILE = None
+CERTFILE = '/home/tpapad01/etc/server.pem'
+# CERTFILE = None
 LOGFILE = '/var/log/jServe.log'
 
 __version__ = "0.0.1"
@@ -19,20 +18,16 @@ __version__ = "0.0.1"
 
 class jServe(threading.Thread):
     '''
-        Process a json request from client, route to callback function, return json (or XML) to client
+        Process a json request from client, route to callback function, return json to client
 
-        Parameters:
+        Args:
             callbacks (dict): Dictionary to use as callback registry *see add_callback()*
             port(int): Port number to use
             host(str): Hostname for server
-            certfile(str): File to use for SSL *(if 'None' SSL will not be used)*
+            certfile(str): File to use for SSL *(if * **None** *SSL will not be used)*
 
-        Attributes
-        ----------
-                jServe.output = 'json' || 'xml' - default is json, controls type of output to return
-
-        Methods
-        -------------------------------------------------------------------------------
+        Attributes:
+            output = 'json | xml' : default is json, type of output to return
         '''
 
 
@@ -48,13 +43,22 @@ class jServe(threading.Thread):
 
         '''
 
-        print ("jServ exit: received signal",signal,file = self.logfile)
-        self.logfile.flush()
+        self.logger("jServ exit: received signal")
+
         self.server.close()
         sys.exit(signal)
 
-    def __init__(self, callbacks = {}, port=PORT , host=HOST, certfile = CERTFILE, logfile = LOGFILE):
+    def logger (self, *msgs ):
+        for msg in msgs:
+            print (msg, file = self.logfile, end = '',flush = False )
+            print (' ', file = self.logfile, end = '',flush = False )
+        print ("", file = self.logfile, flush=True,  )
+
+
+    def __init__(self, callbacks = {}, port=PORT , host=HOST, certfile = CERTFILE, logto = LOGFILE):
         import signal
+        self.logto = logto
+        self.logfile = open(self.logto, "a")
         signal.signal(signal.SIGINT,self.sig_exit)
         threading.Thread.__init__(self)
         self.port = port
@@ -65,19 +69,12 @@ class jServe(threading.Thread):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if certfile is None:
             self.ssl = False
+            self.logger('Not using SSL' )
         else:
             self.ssl = True
-        if logfile.upper() == "STDOUT":
-            self.logfile = sys.stdout
-        else:
-            if logfile.upper() == "STDERR":
-                self.logfile = sys.stderr
-            else:
-                self.logfile = open(LOGFILE, "a")
+            self.logger('Certfile: ' + self.certfile )
+        self.logger (datetime.now().strftime("jServe object initialized: %A %B %d %Y %I:%M %p %Z"))
 
-        print ("\n",file = self.logfile)
-        print (datetime.now().strftime("jServe object initialized: %A %B %d %Y %I:%M %p %Z"),file = self.logfile)
-        self.logfile.flush()
 
 
     def start (self):
@@ -88,19 +85,18 @@ class jServe(threading.Thread):
             None
         Returns:
             None
-
         '''
         try:
             self.server.bind((self.host, self.port))
-            print (datetime.now().strftime("jServe started: %A %B %d %Y %I:%M %p %Z"),file = self.logfile)
+            self.logger (datetime.now().strftime("jServe started: %A %B %d %Y %I:%M %p %Z"))
         except socket.error:
-            print('Bind failed %s' % (socket.error), file = self.logfile)
+            self.logger('Bind failed %s' % (socket.error))
             sys.exit(1)
 
-        self.logfile.flush()
+
         self.server.listen(10)
         if self.ssl:
-            logger (self.certfile)
+            self.logger (self.certfile)
             self.ssl = ssl.wrap_socket(self.server, server_side = True, certfile=self.certfile, keyfile=self.certfile)
         self.run()
 
@@ -117,11 +113,10 @@ class jServe(threading.Thread):
             counter (int): How many times method path did a call back
 
         Returns:
-            path(str),json(json), status(int), counter(int)+1: Returns tupple with path, json object, status, and counter
-
+            path(str),json(json), status(int), counter(int): Returns tupple with path, json object, status, and counter
         '''
-        print ("Callback not registered",file = self.logfile)
-        self.logfile.flush()
+        self.logger ("Callback not registered")
+
         return (path, {}, 404, counter + 1)
 
 
@@ -134,16 +129,13 @@ class jServe(threading.Thread):
             path (re): Path in URL after host '/.*', regular expression
             cfunc(function):  call back function refernce pointer (see default_callback() for details)
 
-        Other:
-            callback registry = {Method: [ {path : callback}, ]}
 
-        Note:
-            user defined functions need to increment counter inside callback set default_callback()
+        Note user defined functions need to increment counter
 
         '''
 
-        print ("Call back add: ",method,path," callback: ",cfunc.__name__, file = self.logfile)
-        self.logfile.flush()
+        self.logger ("Call back add: ",method,path,cfunc)
+
         if method not in self.callregistry:
             self.callregistry[method] = []
         self.callregistry[method].append({path : cfunc })
@@ -156,18 +148,16 @@ class jServe(threading.Thread):
             method(str): Method from URL, used as primary key in callbacks
             path(str): path from URL
             jdata(json):
-
         '''
         status=200
         counter=0
         if method in self.callregistry:
             for m in self.callregistry[method]:
                 for rep in m.keys():
-                    # print ("rep = {} path = {}".format(rep, path), file = self.logfile)
                     if re.fullmatch(rep,path,re.I):
-                        print ("MATCH rep = {} path = {}".format(rep, path), file = self.logfile)
+                        self.logger ("MATCH rep = {} path = {}".format(rep, path))
                         path, jdata, status, counter = m[rep](path,jdata,status=status, counter=counter)  #return first match for now
-                        self.logfile.flush()
+
             if counter != 0:
                 return (path,jdata,status,counter)
         return self.default_callback(path,jdata,status,counter)
@@ -181,46 +171,56 @@ class jServe(threading.Thread):
             addr(list): TCPIP address Information
         Returns:
             sends response to Socket
-
         '''
-        print('Client connected with ' + addr[0] + ':' + str(addr[1]),file = self.logfile)
-        self.logfile.flush()
+        self.logger('Client connected with ' + addr[0] + ':' + str(addr[1]))
+
         try:
             data = conn.recv(4096)
+            self.logger ("data =",data)
         except Exception as err:
             data=""
+            self.logger ("data = NULL")
+        try:
+            self.logger ('Parsing URL')
+            url = urlparse(data)
+        except:
+            self.logger ('ERROR: Could not parse URL properly\nData =')
+            self.logger (data)
 
-        print ("RAW DATA:\n",data,"\n",file = self.logfile)
-        method, wpath, jdata = self.process_url (data.decode('ascii'))
+            conn.close
+            return False
+        self.logger ("URL:",url)
+
+        method, wpath, jdata = self.process_url (url)
         m1, rjdata, rcode, m2 = self.callback (method, wpath, jdata)
-        print ("%s:%s => %s %s %s" % ( addr[0],str(addr[1]),method, wpath, json.dumps(jdata)),file = self.logfile)
-        self.logfile.flush()
-        retdata = json.dumps(rjdata)
-        if self.output == 'xml':
-            retdata = dicttoxml(rjdata).decode()
-        conn.sendall(bytes(self.http_reply(retdata,rcode),'utf-8'))
-        print ("closing connection from: "  + addr[0] + ':' + str(addr[1] ), file = self.logfile)
-        self.logfile.flush()
+        self.logger ("%s:%s => %s %s %s" % ( addr[0],str(addr[1]),method, wpath, json.dumps(jdata)))
+
+        conn.sendall(bytes(self.http_reply(json.dumps(rjdata),rcode),'utf-8'))
+        self.logger ("closing connection from: "  + addr[0] + ':' + str(addr[1] ))
+
         conn.close() # Close
+        return True
 
     def run(self):
         '''
         Opens connection, and creates Thread object to handle requests
 
         '''
-        print('Waiting for connections on: %s port: %s' % (self.host,self.port),file = self.logfile)
-        self.logfile.flush()
+        self.logger('Waiting for connections on port %s' % (self.port))
+
         # We need to run a loop and create a new thread for each connection
         while True:
           try:
               if self.ssl:
                   conn, addr = self.ssl.accept()
+                  self.logger ("Using SSL connection")
               else:
+                  self.logger ("Using Unsecure connection")
                   conn, addr = self.server.accept()
               threading.Thread(target=self.run_url_thread, args=(conn, addr)).start()
           except Exception as err:
-            print (err, file = self.logfile)
-        self.logfile.flush()
+            self.logger (err)
+
 
     def http_reply(self,data,status=200):
         '''
@@ -231,7 +231,6 @@ class jServe(threading.Thread):
             status(int): Status to return see stats table below
         Returns:
             HTTPD respnose string
-
         '''
         stats={  100 : "Continue", 101 : "Switching Protocols",
                  200 : "OK", 201 : "Created", 202 : "Accepted", 203 : "Non-Authoritative Information",
@@ -255,78 +254,17 @@ class jServe(threading.Thread):
         resp += "Server: jServ.py\n"
         resp += "Cache-Control: no-cache\n"
         resp += "Content-Length: {}\n".format(len(data))
+        resp += "Access-Control-Allow-Origin: *\n"  #Added to avoid CORS issue
         resp += "Content-Type: application/json; charset=utf-8\n"
         resp += "Connection: Closed\n\n"
         resp += data + "\n"
-        print ("Respone:\n",resp,file=self.logfile)
-        self.logfile.flush()
+        self.logger ("Respone:")
+        self.logger (resp)
+
         return resp
 
-    def __process_path(self,data):
-        try:
-            pstr = re.search('.*({.*}).*',data).group(1)
-        except:
-            return "{}"
-        print ("PATH PARAMS = ",pstr, file = self.logfile)
-        return pstr
 
-    def __process_query(self,data):
-        # qparams = args.query.decode('ascii').split()[0].replace("="," : ").replace("&"," , ")
-        qry = data.split()[1]
-        if '?' in qry:
-            qry = qry.split('?')[1]
-        if '&' in qry:
-            qparams = qry.split()[0].split('&')
-            qstr = "{"
-            sep = ""
-            for i in qparams:
-                p = i.split("=")
-                qstr = qstr + sep + '"' + p[0] + '" : '
-                if len(p) > 1:
-                    qstr = qstr + '"' + unquote_plus(p[1]) + '"'
-                sep = ", "
-            qstr = qstr + "}"
-        else:
-            qstr = "{}"
-        print ("GET PARAMS = ",qstr, file = self.logfile)
-        return qstr
-
-
-
-    def __process_params(self,data):
-        print ("DATA = ",data)
-        if "form-data;" in data:
-            data = data.replace("\r\n"," ").replace('\n','\\n')
-#            data = re.search('^.*(form-data\;.*$)',re.sub(' +',' ',data.replace("\r"," ").replace("\n"," ")),re.MULTILINE).group(1)
-            urlst = []
-            for u in data[re.search ('name=',data).start():].split('name='):
-                if u:
-                    u = u[:re.search('\-\-\-',u).start()]
-                    u = u[1:]
-                    m = re.search('"',u).start()
-                    p1 = unquote_plus(u[:m].strip())
-                    p2 = unquote_plus(u[m+1:].strip())
-                    urlst.append('"'+p1+'" : "' + p2 + '"')
-            pstr = "{" + ' , '.join(urlst) + "}"
-            print ("FORM DATA = ", pstr)
-            return pstr
-        else:
-            return '{}'
-
-    def process_url (self,data):
-        #URL = data.decode('ascii')
-        method = data.split()[0]
-        wpath = data.split()[1]
-        if '&' in wpath:
-            wpath = wpath.split('&')[0]
-        jstr = {}
-        jstr.update(json.loads(self.__process_path(data)))
-        jstr.update(json.loads(self.__process_query(data)))
-        jstr.update(json.loads(self.__process_params(data)))
-        print  (method,wpath, jstr, file = self.logfile)
-        return (method,wpath, jstr)
-
-    def process_url_old (self,args):
+    def process_url (self,args):
         '''
         Break up URL string recieved in method, path, json
 
@@ -336,6 +274,9 @@ class jServe(threading.Thread):
         Returns:
             method(str), path(str), json(json): tupple with data to be used for callbacks
         '''
+        self.logger('process url')
+        self.logger("ARGS =",args, args.path)
+
         def is_number(j):
             try:
                 if float(j):
@@ -356,98 +297,51 @@ class jServe(threading.Thread):
                 else:
                     j = sep + k + sep
                 retstr =  retstr + " " + j
-            retstr = re.sub('%20',' ',retstr)
             return retstr
 
-        def retDefined(p1,p2,p3):
-            print (p1,p2,p3)
-            pr = p1
-            if len(pr) == 0:
-                pr = p2
-            if len(pr) == 0:
-                pr = p3
-            return pr
-
-        print ("URL = ", args, file = self.logfile)
-        urlst = args.path.decode('ascii').strip().replace("["," [ ").replace("]"," ] ").replace("{"," { ").replace("}"," } ").replace(":"," : ").replace(","," , ").split()
+        urlst = args.path.decode('ascii').replace("["," [ ").replace("]"," ] ").replace("{"," { ").replace("}"," } ").replace(":"," : ").replace(","," , ").split()
         method = urlst[0]
         wpath = urlst[1]
-        print ("method = ", method, " wpath = ", wpath, file = self.logfile)
-        self.logfile.flush()
-        qparams = []
-        if args.query:
-            qparams = args.query.decode('ascii').split()[0].replace("="," : ").replace("&"," , ")
-            p = qparams.split(',')
-            qparams = ''
-            for i in p:
-                if ':' not in i:
-                    i = i + ' : true '
-                qparams = qparams + i + ' , '
-            qparams = qparams[:-3]
-            # print (params, file = self.logfile)
-            qparams = qparams.split()
-        prparams = []
-        if args.params:
-            if "form-data;" in args.params.decode('ascii'):
-                urlst = re.sub(' +',' ',args.params.decode('ascii').replace("\r"," ").replace("\n"," ")[1:])
-                urlst = urlst.split("form-data; ")
-                for i in urlst:
-                    if 'name=' not in i:
-                        urlst.remove(i)
-                print (urlst)
-                for i in range(0,len(urlst)):
-                    urlst[i] = re.sub('\-\-\-.*','',urlst[i]).strip().replace('name="','').replace('"',':')
-                    u = urlst[i].split(":")
-                    print (u)
-                    u[1] = re.sub(' ','%20',u[1].strip())
-                    urlst[i] = ' : '.join(u)
-                    print (u)
-                prparams = ' , '.join(urlst).split()
-                print (prparams)
-        ptparams = []
-        if args.path:
-            #try:
-            print (args.path)
-            #urlst = args.path.decode('ascii').replace("{"," { ").replace("}"," } ").replace(":"," : ").replace(","," , ").split()
-            urlst = re.sub("^.*?{","",args.path.decode('ascii'))
-            urlst = re.sub('}$','',urlst).replace('",','" ,"')
-            u1 = urlst.split(" , ")
-            for i in range(0,len(u1)):
-                u2 = u1[i].split(":")
-                u2[1] = re.sub(" ","%20",u2[1])
-                u1[i] = ':'.join(u2)
-            urlst = ','.join(u1)
-            print (urlst)
-            #ptparams = urlst[urlst.index("{")+1:urlst.index("}",-1)]
-            ptparams = urlst.split(',')
-            print (ptparams)
-            #except:
-            #   pass
+        self.logger ("method = ", method, " wpath = ", wpath)
 
-        if method in ["GET","DELETE"]:
-            params = retDefined ( qparams, ptparams, prparams)
-        if method in ["POST","PUT"]:
-            params = retDefined (ptparams, prparams, qparams )
+        if method == "GET":
+            if args.query:
+                params = args.query.decode('ascii').split()[0].replace("="," : ").replace("&"," , ")
+                p = params.split(',')
+                params = ''
+                for i in p:
+                    if ':' not in i:
+                        i = i + ' : true '
+                    params = params + i + ' , '
+                params = params[:-3]
+                params = params.split()
 
+            else:
+                params = []
+        else:
+            if args.query:
+                ags = args.query
+            else:
+                ags = args.path
+            try:
+                urlst = ags.decode('ascii').replace("{"," { ").replace("}"," } ").replace(":"," : ").replace(","," , ").split()
+                params = urlst[urlst.index("{")+1:urlst.index("}",-1)]
+            except:
+                params = []
         jstr = "{ " + join_escaped(params) + " }"
-        print (jstr,file = self.logfile)
-        self.logfile.flush()
+        self.logger (jstr)
         return (method,wpath, json.loads(jstr))
 
 if __name__ == '__main__':
     ''' Below is an example and not called if imported as module '''
     def my_call (path, jdata, status=400, counter=0):
-        # print ("my_call: \n\t===>>> ",jdata)
         jdata['more'] = 'testing'
         jdata['counter'] = counter
-        # print ("\n\t\t===>>> ",jdata)
         return (path, jdata, status, counter + 1)
 
-    myjs = jServe(logfile="stderr")
-    # myjs.output = 'xml'
+    myjs = jServe()
     myjs.add_callback("GET","/tes.*",my_call)
     myjs.add_callback("POST","/te.*",my_call)
     myjs.add_callback("POST","/t.*",my_call)
     myjs.add_callback("PUT","/t.*",my_call)
-    # print (myjs.callregistry)
     myjs.start()
